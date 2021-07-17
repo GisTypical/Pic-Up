4b5410086d3bcfeec94820f48617566d971c5c73
Gerardo <gerardopapaleco@gmail.com>
2021-07-17 00:38:40 -0400

    testing routes gunicorn 2

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__, static_folder='./frontend/build', static_url_path='/')
 app.secret_key = 'qid'
 
 
-@app.get('/')
+@app.router('/', methods=['GET'])
 def index():
     return app.send_static_file('index.html')
 
