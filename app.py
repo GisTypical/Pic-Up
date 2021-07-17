f7343556f538cbbb461f492894778ee9ca4cf133
Gerardo <gerardopapaleco@gmail.com>
2021-07-17 00:21:47 -0400

    First Commit - Setting up

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, session
+
+app = Flask(__name__, static_folder='./frontend/build', static_url_path='/')
+
+app.secret_key = 'qid'
+
+
+@app.get('/')
+def index():
+    return app.send_static_file('index.html')
+
+
+@app.route('/<string:name>')
+def hello_world(name):
+    if not 'name' in session:
+        print('no hay cookies')
+        session['name'] = name
+    else:
+        print('si hay')
+    return f'<p align=\"center\">Hello, {session["name"]}!</p>'
