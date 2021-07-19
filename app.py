a6e4bd40b69a6bec724d74be313bd3417769f635
Gerardo <gerardopapaleco@gmail.com>
2021-07-19 03:23:40 -0400

    404 handling for react-router

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
-from flask import Flask, session
+from flask import Flask
 
 app = Flask(__name__, static_folder='./frontend/build', static_url_path='/')
 
 app.secret_key = 'qida'
+
+
+@app.errorhandler(404)
+def not_found(e):
+    print("hola")
+    return app.send_static_file('index.html')
 
 
 @app.route('/', methods=['GET'])
@@ -10,11 +16,6 @@ def index():
     return app.send_static_file('index.html')
 
 
-@app.route('/api/<string:name>')
-def hello_world(name):
-    if not 'name' in session:
-        print('no hay cookies')
-        session['name'] = name
-    else:
-        print('si hay')
-    return f'<p align=\"center\">Hello, {session["name"]}!</p>'
+@app.route('/api/hola')
+def hello_world():
+    return 'saludos usuario'
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,37 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 function App() {
+  const [hola, setHola] = useState("");
+  useEffect(() => {
+    axios.get("/api/hola").then(({ data }) => {
+      setHola(data);
+    });
+  }, [setHola]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav style={{ marginLeft: "10px" }}>
+        <ul>
+          <li>
+            <Link to="/">to Principal</Link>
+          </li>
+          <li>
+            <Link to="/hola">to Hola</Link>
+          </li>
+          <li>
+            <Link to="/ruta2">to Ruta2</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route exact path="/">
+          Principal
+        </Route>
+        <Route path="/hola">{hola}</Route>
+        <Route path="/ruta2">Ruta2</Route>
+      </Switch>
+    </Router>
   );
 }
 
